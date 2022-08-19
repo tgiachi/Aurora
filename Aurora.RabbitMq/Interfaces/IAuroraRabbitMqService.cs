b05b8047655b5d25e365b3b2cfb008530ecf115b
Tom <squid@stormwind.it>
2022-08-19 11:34:55 +0200

    Added RabbitMq library

---
+++ Aurora.RabbitMq/Interfaces/IAuroraRabbitMqService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurora.RabbitMq.Interfaces
+{
+    public interface IAuroraRabbitMqService : IDisposable
+    {
+        Task Subscribe<TEntity>(string queueName, Action<TEntity> action);
+
+        Task Publish<TEntity>(string queueName, TEntity entity);
+    }
+}
